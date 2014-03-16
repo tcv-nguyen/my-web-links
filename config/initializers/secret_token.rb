9e8939645ae5ad0c6a62db6c692bdd7aa1852d6a
Tony Nguyen <tcv.nguyen.2013@gmail.com>
2014-03-15 19:49:28 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyWebLinks::Application.config.secret_key_base = '71b6cce8a00721aa27ce6f9a12ac2a306b3fe43188751ecf1a6ca85bdbf5147b9c96eb96cc79624a1c338e165eb0efb6a7cd3571dfe628327b3c407f0147ba2f'
